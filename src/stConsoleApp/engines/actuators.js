716b238805230ea9b860d1fe0eb8aae2cde91f03
UnlevelTeam <nobody@nowhere.waw.net>
2016-07-22 10:53:25 +0200

    Working on ST Console App
    - Added more SCS_Requests to engines

--- src/stConsoleApp/engines/actuators.js
+++ src/stConsoleApp/engines/actuators.js
@@ -71,11 +71,131 @@ function get_ActuatorsList(options) {
 }
 
 
+/**
+ * Get Actuator Info
+ * 
+ * @memberof st.consoleApp.engines.actuators
+ * @public
+ * 
+ * @param {object} options - Options
+ * @param {st.consoleApp.ConsoleApp} options.consoleApp - Console App reference
+ * @param {string} options.actuatorID - Actuator ID
+ * 
+ * @returns {Promise}
+ * 
+ */
+function get_ActuatorInfo(options) {
+	
+	let _consoleApp = options.consoleApp;
+	let _SCSRequestsForEngines = _consoleApp.engines.SCS_Requests;
+	
+	console.log("<~i~> st.consoleApp.engines.actuators.get_ActuatorInfo");	// TODO REMOVE DEBUG LOG
+	console.log(_consoleApp);	// TODO REMOVE DEBUG LOG
+	
+	
+	let promise = new Promise(function(resolve, reject) {
+		
+		_SCSRequestsForEngines.get_ActuatorInfo({
+			
+			"scsClient": _consoleApp.scsClient,
+			"actuatorID": options.actuatorID,
+			
+			"_onComplete": function(data) {
+				
+				console.log("<~i~> st.consoleApp.engines.actuators.get_ActuatorInfo._onComplete");	// TODO REMOVE DEBUG LOG
+				console.log(data);	// TODO REMOVE DEBUG LOG
+
+			}
+		
+		}).then(function(value) {
+			
+			console.log("<~i~> st.consoleApp.engines.actuators.get_ActuatorInfo.then");	// TODO REMOVE DEBUG LOG
+			console.log(value);	// TODO REMOVE DEBUG LOG
+			
+			resolve(value);
+			
+		}, function(reason) {
+			
+			console.log("<~i~> st.consoleApp.engines.actuators.get_ActuatorInfo.error");	// TODO REMOVE DEBUG LOG
+			console.log(reason);	// TODO REMOVE DEBUG LOG
+			
+			reject(Error("It broke"));
+		});
+		
+	});
+	
+	return promise;
+}
+
+
+/**
+ * Get Actuator Options
+ * 
+ * @memberof st.consoleApp.engines.actuators
+ * @public
+ * 
+ * @param {object} options - Options
+ * @param {st.consoleApp.ConsoleApp} options.consoleApp - Console App reference
+ * @param {string} options.actuatorID - Actuator ID
+ * 
+ * @returns {Promise}
+ * 
+ */
+function get_ActuatorOptions(options) {
+	
+	let _consoleApp = options.consoleApp;
+	let _SCSRequestsForEngines = _consoleApp.engines.SCS_Requests;
+	
+	console.log("<~i~> st.consoleApp.engines.actuators.get_ActuatorOptions");	// TODO REMOVE DEBUG LOG
+	console.log(_consoleApp);	// TODO REMOVE DEBUG LOG
+	
+	
+	let promise = new Promise(function(resolve, reject) {
+		
+		_SCSRequestsForEngines.get_ActuatorOptions({
+			
+			"scsClient": _consoleApp.scsClient,
+			"actuatorID": options.actuatorID,
+			
+			"_onComplete": function(data) {
+				
+				console.log("<~i~> st.consoleApp.engines.actuators.get_ActuatorOptions._onComplete");	// TODO REMOVE DEBUG LOG
+				console.log(data);	// TODO REMOVE DEBUG LOG
+
+			}
+		
+		}).then(function(value) {
+			
+			console.log("<~i~> st.consoleApp.engines.actuators.get_ActuatorOptions.then");	// TODO REMOVE DEBUG LOG
+			console.log(value);	// TODO REMOVE DEBUG LOG
+			
+			resolve(value);
+			
+		}, function(reason) {
+			
+			console.log("<~i~> st.consoleApp.engines.actuators.get_ActuatorOptions.error");	// TODO REMOVE DEBUG LOG
+			console.log(reason);	// TODO REMOVE DEBUG LOG
+			
+			reject(Error("It broke"));
+		});
+		
+	});
+	
+	return promise;
+}
+
+
+
+
 let _lib = {
 	"get_ActuatorsList": get_ActuatorsList,
+	"get_ActuatorInfo": get_ActuatorInfo,
+	"get_ActuatorOptions": get_ActuatorOptions,
 	
 	"_public": {
-		"get_ActuatorsList": get_ActuatorsList
+		"get_ActuatorsList": get_ActuatorsList,
+		"get_ActuatorInfo": get_ActuatorInfo,
+		"get_ActuatorOptions": get_ActuatorOptions
 	}
 		
 };
--- src/stConsoleApp/engines/sensors.js
+++ src/stConsoleApp/engines/sensors.js
@@ -70,15 +70,132 @@ function get_SensorsList(options) {
 }
 
 
+/**
+ * Get Sensor Info
+ * 
+ * @memberof st.consoleApp.engines.sensors
+ * @public
+ * 
+ * @param {object} options - Options
+ * @param {st.consoleApp.ConsoleApp} options.consoleApp - Console App reference
+ * @param {string} options.sensorID - Sensor ID
+ * 
+ * @returns {Promise}
+ * 
+ */
+function get_SensorInfo(options) {
+	
+	let _consoleApp = options.consoleApp;
+	let _SCSRequestsForEngines = _consoleApp.engines.SCS_Requests;
+	
+	console.log("<~i~> st.consoleApp.engines.sensors.get_SensorInfo");	// TODO REMOVE DEBUG LOG
+	console.log(_consoleApp);	// TODO REMOVE DEBUG LOG
+	
+	
+	let promise = new Promise(function(resolve, reject) {
+		
+		_SCSRequestsForEngines.get_SensorInfo({
+			
+			"scsClient": _consoleApp.scsClient,
+			"sensorID": options.sensorID,
+			
+			"_onComplete": function(data) {
+				
+				console.log("<~i~> st.consoleApp.engines.sensors.get_SensorInfo._onComplete");	// TODO REMOVE DEBUG LOG
+				console.log(data);	// TODO REMOVE DEBUG LOG
+
+			}
+		
+		}).then(function(value) {
+			
+			console.log("<~i~> st.consoleApp.engines.sensors.get_SensorInfo.then");	// TODO REMOVE DEBUG LOG
+			console.log(value);	// TODO REMOVE DEBUG LOG
+			
+			resolve(value);
+			
+		}, function(reason) {
+			
+			console.log("<~i~> st.consoleApp.engines.sensors.get_SensorInfo.error");	// TODO REMOVE DEBUG LOG
+			console.log(reason);	// TODO REMOVE DEBUG LOG
+			
+			reject(Error("It broke"));
+		});
+		
+	});
+	
+	return promise;
+}
+
+
+/**
+ * Get Sensor Options
+ * 
+ * @memberof st.consoleApp.engines.sensors
+ * @public
+ * 
+ * @param {object} options - Options
+ * @param {st.consoleApp.ConsoleApp} options.consoleApp - Console App reference
+ * @param {string} options.sensorID - Sensor ID
+ * 
+ * @returns {Promise}
+ * 
+ */
+function get_SensorOptions(options) {
+	
+	let _consoleApp = options.consoleApp;
+	let _SCSRequestsForEngines = _consoleApp.engines.SCS_Requests;
+	
+	console.log("<~i~> st.consoleApp.engines.sensors.get_SensorOptions");	// TODO REMOVE DEBUG LOG
+	console.log(_consoleApp);	// TODO REMOVE DEBUG LOG
+	
+	
+	let promise = new Promise(function(resolve, reject) {
+		
+		_SCSRequestsForEngines.get_SensorOptions({
+			
+			"scsClient": _consoleApp.scsClient,
+			"sensorID": options.sensorID,
+			
+			"_onComplete": function(data) {
+				
+				console.log("<~i~> st.consoleApp.engines.sensors.get_SensorOptions._onComplete");	// TODO REMOVE DEBUG LOG
+				console.log(data);	// TODO REMOVE DEBUG LOG
+
+			}
+		
+		}).then(function(value) {
+			
+			console.log("<~i~> st.consoleApp.engines.sensors.get_SensorOptions.then");	// TODO REMOVE DEBUG LOG
+			console.log(value);	// TODO REMOVE DEBUG LOG
+			
+			resolve(value);
+			
+		}, function(reason) {
+			
+			console.log("<~i~> st.consoleApp.engines.sensors.get_SensorOptions.error");	// TODO REMOVE DEBUG LOG
+			console.log(reason);	// TODO REMOVE DEBUG LOG
+			
+			reject(Error("It broke"));
+		});
+		
+	});
+	
+	return promise;
+}
 
 
 
 
 let _lib = {
 	"get_SensorsList": get_SensorsList,
+	"get_SensorInfo": get_SensorInfo,
+	"get_SensorOptions": get_SensorOptions,
+
 	
 	"_public": {
-		"get_SensorsList": get_SensorsList
+		"get_SensorsList": get_SensorsList,
+		"get_SensorInfo": get_SensorInfo,
+		"get_SensorOptions": get_SensorOptions
 	}
 		
 };
